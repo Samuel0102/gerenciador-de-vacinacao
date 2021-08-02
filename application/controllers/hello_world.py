0652ea8e4fd5f629e892302f790fce07964905ab
Samuel0102 <damu.comsa@gmail.com>
2021-08-02 13:16:04 -0300

    alterando arquivos css

--- application/controllers/hello_world.py
+++ application/controllers/hello_world.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/user-register")
+def user_register():
+    return render_template("cadastro_de_usuarios.html")
